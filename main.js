5ca442a40f53a19b588bbadd86edc83b7cdf84d0
Summer <swatson9008@yahoo.com>
2022-09-27 12:20:13 -0400

    created the function and variables to make a grid

---
+++ main.js
@@ -0,0 +1,32 @@
+
+//grid variables
+const container = document.getElementById("mainContainer");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+
+//lets make a row
+function makeRows(rowNum){
+    for (r = 0; r < rowNum; r++){
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    }
+}
+
+//lets make a column
+function makeColumns (cellNum){
+    for (i = 0; i < rows.length; i++){
+        for (g = 0; g < cellNum; g++){
+            let newCell = document.createElement("div");
+            rows[g].appendChild(newCell).className = "cell";
+        }
+    }
+}
+
+//lets make a standard sized grid
+function standardGrid () {
+    makeRows(16);
+    makeColumns(16);
+}
+
+standardGrid();
