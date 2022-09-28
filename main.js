d85e8347dec63167b74114e1cea94b02e96140e0
Summer <swatson9008@yahoo.com>
2022-09-28 15:37:38 -0400

    created a working drawing board and clear button

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const container = document.getElementById("mainContainer");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
+const clearButton = document.getElementById("clearbtn");
+
 
 //lets make a row
 function makeRows(rowNum){
@@ -13,15 +15,28 @@ function makeRows(rowNum){
     }
 }
 
-//lets make a column
+//lets make a column 
 function makeColumns (cellNum){
     for (i = 0; i < rows.length; i++){
         for (g = 0; g < cellNum; g++){
             let newCell = document.createElement("div");
             rows[g].appendChild(newCell).className = "cell";
+            /// function that detects mouseover and changes color
+            newCell.addEventListener('mouseover', changeColor)
+            function changeColor(){
+                newCell.style.backgroundColor="black";
+            }
+
+            //clear canvas
+            clearButton.addEventListener('click', clearCanvas);
+            function clearCanvas(){   
+                newCell.style.backgroundColor="white"};
+            
         }
     }
 }
+
+
 
 //lets make a standard sized grid
 function standardGrid () {
@@ -29,4 +44,11 @@ function standardGrid () {
     makeColumns(16);
 }
 
+//makes a grid
 standardGrid();
+
+
+
+
+
+
