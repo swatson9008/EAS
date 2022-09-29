cda8ec696ebafac4d2a3ae5c9bf3ae539b1bea24
Summer <swatson9008@yahoo.com>
2022-09-28 22:35:17 -0400

    finished canvas main properties

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-
 //grid variables
+
 const container = document.getElementById("mainContainer");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
 const clearButton = document.getElementById("clearbtn");
+const gridChange = document.getElementById("gridSize");
 
+//let startingSize = 30;
 
 //lets make a row
 function makeRows(rowNum){
@@ -47,8 +49,35 @@ function standardGrid () {
 //makes a grid
 standardGrid();
 
+function clearGrid() {
+    const gridMap = Array.from(container.childNodes);
+    gridMap.forEach((element) => {
+      container.removeChild(element);
+    });
+  }
 
+gridChange.addEventListener('click', sizeChange);
+function sizeChange(){
+    let newSize = prompt("Enter the size of the canvas");
+    let wantedSize = parseInt(newSize);
+    if (wantedSize <= 100 && wantedSize > 0) {
+       //wantedSize = startingSize;
+       function alteredGrid(){
+        makeRows(wantedSize)
+        makeColumns(wantedSize)
+       };
+       clearGrid();
+       alteredGrid();
+    }
+    if (wantedSize > 100){
+        alert("Please choose a smaller size")
+    }
+    else if (isNaN(wantedSize)){
+        alert("Please enter a number between 1 and 100")
+    }
+}
 
-
-
-
+/*function alteredGrid (){
+    makeRows(startingSize);
+    makeColumns(startingSize);
+}*/
